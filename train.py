9ea84a8bce8976750a6efca20825014198cd7994
Abdur Rafay Akbar <abdurrafaym10@gmail.com>
2026-08-17 20:24:22 +0500

    Visualized the Data using Bar chart

--- train.py
+++ train.py
@@ -10,10 +10,28 @@ shape=df.shape
 missing_values=df.isnull().sum()
 duplicate_values=df.duplicated().sum()
 
-print(head)
-print(tail)
-print(desc)
-print(info)
-print(shape)
-print(missing_values)
-print(duplicate_values)
+# print(head)
+# print(tail)
+# print(desc)
+# print(info)
+# print(shape)
+# print(missing_values)
+# print(duplicate_values)
+
+# MATPLOT
+
+import matplotlib.pyplot as plt
+
+category_count=df["Category"].value_counts()
+
+plt.figure(figsize=(8,5))
+
+plt.bar(category_count.index,category_count.values)
+
+plt.title("Complaint Categorizing")
+
+plt.xlabel("Category")
+
+plt.ylabel("Number of Complaints")
+
+plt.show()
